eadf94bcedc80b3e852642c4bbd0c3072eaf2eec
DUDEKULA KHASIM <122206488+D-khasim@users.noreply.github.com>
2025-07-22 14:50:15 +0530

    Update DataApi.js

--- src/assets/DataApi.js
+++ src/assets/DataApi.js
@@ -1,4 +1,4 @@
-export const youtube_api='AIzaSyBrFCSe8heuj7Uev9rSnwmasGODXSQMBss'
+export const youtube_api='Your-Api-Key'
 export const calculate =(views)=>{
     if(views>=1000000) return Math.floor(views/1000000)+'M'
     else if(views>=1000) return Math.floor(views/1000)+'K'
